8660324f9288d35a8c3e945559be2e37362ddc56
Gayashan Amarasinghe <chozrc@gmail.com>
2013-09-15 00:21:43 +0530

    Use PerfDataViewer to process pical data.

---
+++ utilities/process_pical_data.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+#
+
+#../data_viewer/PerfDataViewer.py ppical-good ../data/ppc/ppical-good_2_100000000.txt
+
+PYTHON=python
+SCRIPT="../data_viewer/PerfDataViewer.py"
+data=(100000000 500000000 1000000000)
+power7reducedevents="r2,r3c046,r2c048,r2f080,r26080,r30881,r26182,r26880,rd0a2,rd0a0"
+PROGRAM=(ppical-good ppical-bad_fs)
+
+for i in 1 2 4 8 16
+do
+    echo "------------------------------------------------"
+    echo "No of threads ${i}"
+    for d in ${data[@]}; do
+        echo $d
+        for program in ${PROGRAM[@]}; do
+            echo $program 
+            $PYTHON $SCRIPT ${program} "../data/ppc/${program}_${i}_${d}.txt"
+            echo
+        done
+    done
+    echo
+done
+
